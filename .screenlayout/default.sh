0559296659bd52028947e22adb57f8a0fa4f4942
cantorsdustbunnies <kclarencestratton@gmail.com>
2019-09-05 14:11:56 -0600

    better transparency with kitty, fixed weird error caused by Arch update where Dell U2414h wasn't being seen. The solution was some combination of rebooting, and using nvidia-settings to detect the monitor. Finally cutting power from the monitor and running nvidia-settings again.
    this was a terrible, horrible, no good time because I'm not really sure
    what causes it, and I don't yet know enough about initializing x to know
    what to try.
    
    in the future:
    
        I suspect it's largely a case of the xserver causing the controller
        / drivers of the monitor to wig out - I really tried to RTFM on this
        one but... no such luck.
    
        DISCONNECT THE POWER FROM THE MONITOR AND REBOOT
    
        THE X SERVER. (POSSIBLY THE ENTIRE SYSTEM) RECONNECT POWER AND RUN
        'DETECT DISPLAYS' IN nvidia-settings. YOU MAY HAVE TO GO HAM ON IT
    
        ONCE IT DETECTS, ARRANGE THE MONITORS (THEY'LL LIKELY BE OUT OF
        ALIGNMENT) THEN SAVE THE CONFIGURATION TO .screenlayout/default.sh
    
        AT THIS POINT YOUR MONITOR SHOULD BE CONNECTED BUT WILL HAVE A NEW
        AUTOGENERATED NAME FROM XRANDR -- REMEMBER TO REFLECT THIS CHANGE
        INSIDE YOUR I3CONFIG, OR ANYWHERE ELSE THAT NEEDS TO KNOW THE
        LAYOUT.
    
        IF THAT DOESNT WORK... GOOD LUCK. YOU CAN GATHER LOTS OF INFORMATION
        WITH THE FOLLOWING COMMANDS:
    
        1. ) check inxi -G
        2. ) or more generally: inxi -Fxxxz
        3. ) check mhwd -li
        4. ) try xrandr / autorandr / arandr
        5. ) systemctl | grep displaylink
        6. ) nvidia-settings -q CurrentMetaMode
        7. ) autorandr --force
        8. ) sudo service lightdm stop
        9. ) sudo /etc/init.d/lxdm stop
        10.) lspci -nn -v
    
        Dear god, may it always be a power cycle issue after this.

---
+++ .screenlayout/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-4.1 --primary --mode 1920x1080 --pos 1680x0 --rotate normal --output DVI-D-0 --off --output DP-0 --off --output DP-1 --mode 1680x1050 --pos 0x0 --rotate normal --output HDMI-0 --mode 1600x900 --pos 3600x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
